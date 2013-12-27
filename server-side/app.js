96c57969adbfa9ae25951a9af964c5eba26cfd89
Elixa-Mae Pesa <eapesa@gmail.com>
2013-12-27 19:51:30 +0800

    cleaned codes

--- server-side/app.js
+++ server-side/app.js
@@ -1,24 +1,30 @@
 var express = require('express')
 var http = require('http');
+var nconf = require('nconf');
 var path = require('path');
 
 var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 
+nconf.use('file', {
+    file: '../config.json'
+});
+
 var Tail = require('tail').Tail;
-var tail = new Tail('../test.log');
+var tail = new Tail(nconf.get('logs:path'));
 
 io.sockets.on('connection', function (socket) {
     tail.on('line', function(logs) {
         console.log(logs);
-        socket.emit('news', { hello : logs });
+        socket.emit('logs', { data : logs });
     });
 
-    socket.on('my other event', function (data) {
-        console.log(data);
+    socket.on('response', function (data) {
+        console.log('CLIENT RESPONSE: ' + data);
     });
 });
+
 
 app.configure(function(){
     var dir = path.join(__dirname, '../');
@@ -29,4 +35,6 @@ app.get('/', function(req, res) {
     res.sendfile('/Users/eapesa/Desktop/Voyager_Git/bstream/socket.io/client.html');
 });
 
-server.listen(8080);
+server.listen(nconf.get('app:port'), nconf.get('app:host'), function() {
+    console.log('Server listening on ' + nconf.get('app:host') + ':' + nconf.get('app:port'));
+});
